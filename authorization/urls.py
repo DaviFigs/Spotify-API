8b5ab83cfd78339eab40405fea1202350fdd3ed2
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-12-24 17:44:13 -0400

    Correct url redirect by sessions

--- authorization/urls.py
+++ authorization/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.render_login, name="login"),
     path('auth/', views.authorize, name='auth'),
-    #path('exit_session/', views.exit_session, name="exit_session"),
+    path('clearsession/', views.clearsession, name="clearsession"),
 
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, redirect
 from spotify import authorizate as auths
 
-def render_login(request):   
-    request.session['auth'] = False
-    return render(request,'login.html')
+def render_login(request): 
+    try:
+        if request.session['auth'] == True:
+            return redirect('main_page')
+        else:
+            request.session['auth'] = False
+            return render(request,'login.html')
+        
+    except KeyError:
+        request.session['auth'] = False
+        return render(request, 'login.html')
 
 def authorize(request):
     url = auths.authorize()
     return redirect(f'{url}')
+
+
+def clearsession(request):
+    try:
+        if request.session['auth'] == True:
+            request.session.flush()
+            return redirect('login')
+        else:
+            return redirect('login') 
+    except KeyError:
+        return redirect('login')
+
