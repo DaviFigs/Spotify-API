e60fc6faa0074c1eeccde77f7a35f1eb994fe20d
Davi Figueiredo <devdeoliveira06@gmail.com>
2024-01-07 23:53:47 -0400

    ItemListClass

--- spotify/json_manager/item_classes.py
+++ spotify/json_manager/item_classes.py
@@ -1,5 +1,14 @@
 #Here I'll create classes for itens received by json user data
 
+class ItensList:
+    def __init__(self) -> None:
+        self.currently_list = []
+    
+    def add_item_on_list(self, item):#receive one item, artist or song item
+        self.currently_list.append(item)
+
+    def return_list(self):
+        return self.currently_list
 
 class SongItem:
     pass
