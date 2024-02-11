5181999f2985e90bcc5b77b6bb132f28c40c18f5
Davi Figueiredo <devdeoliveira06@gmail.com>
2024-02-10 23:08:55 -0400

    comment

--- spotify/authorizate.py
+++ spotify/authorizate.py
@@ -4,6 +4,9 @@ import random
 load_dotenv()
 
 def authorize(): #this function will authorize our app to take informations about the logged user
+    #AUTHORIZE_URL = 'https://accounts.spotify.com/authorize'
+    #CALLBACK_URL = 'http://127.0.0.1:8000/app/callback'
+
     auth_url = os.getenv('AUTHORIZE_URL')\
         +'?client_id='+os.getenv('CLIENT_ID')\
         +'&response_type=code'\
